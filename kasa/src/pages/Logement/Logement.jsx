d7ddae447cfbda736e85e117b890b4cd0735d7b2
Diane-deezy <96577844+Diane-deezy@users.noreply.github.com>
2022-10-27 00:02:13 +0200

    Update Logement.jsx

--- kasa/src/pages/Logement/Logement.jsx
+++ kasa/src/pages/Logement/Logement.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import "./Logement.css";
 import {useParams, Navigate} from 'react-router-dom';
 import listeLogements from "../../assets/api/logements.json";
